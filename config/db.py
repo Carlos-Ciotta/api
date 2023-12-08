71875b2536fbd0af25f33a0a894b27a098f231df
Carlos-Ciotta <153217971+Carlos-Ciotta@users.noreply.github.com>
2023-12-08 14:21:20 -0300

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://root:Mysql2023ciotta,*@localhost:3306/db-sistema-pontos-ciotta")
+engine = create_engine("mysql+pymysql://root:AA-FCeC6eH4feEg1d61GB-3a3ff1g-hf@monorail.proxy.rlwy.net:31935/railway")
 
 meta = MetaData()
 
